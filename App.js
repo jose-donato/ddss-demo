b3c7b58fdedcb87ff677f50677799c3b4de235f6
jose-donato <43375532+jose-donato@users.noreply.github.com>
2020-11-04 16:41:15 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Platform } from 'react-native';
+import { Platform, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -7,69 +7,73 @@ import SignIn from './screens/SignIn';
 import SignUp from './screens/SignUp';
 import * as SecureStore from 'expo-secure-store';
 //import Splash from './screens/Splash';
-import Home from "./screens/Home";
+import Home from './screens/Home';
 
 const Stack = createStackNavigator();
 
 function App({ navigation }) {
-    const [user, setUser] = useState("")
-    useEffect(() => {
-        if (Platform.OS === "android" || Platform.OS === "ios") {
-            SecureStore.getItemAsync("user").then(val => {
-                setUser(val)
-            })
-        }
-    }, [])
+  const [jwt, setJwt] = useState('');
+  useEffect(() => {
+    if (Platform.OS === 'android' || Platform.OS === 'ios') {
+      SecureStore.getItemAsync('jwt').then((val) => {
+        setJwt(val);
+      });
+    }
+  }, []);
 
-    async function login(user) {
-        if (Platform.OS === "android" || Platform.OS === "ios") {
-            await SecureStore.setItemAsync("user", user)
-        }
-        setUser(user)
+  async function login(jwt) {
+    //make api call to login
+    if (Platform.OS === 'android' || Platform.OS === 'ios') {
+      await SecureStore.setItemAsync('jwt', jwt);
     }
-    async function logout() {
-        if (Platform.OS === "android" || Platform.OS === "ios") {
-            await SecureStore.deleteItemAsync("user")
-        }
-        setUser("")
+    setJwt(jwt);
+  }
+  async function logout() {
+    if (Platform.OS === 'android' || Platform.OS === 'ios') {
+      await SecureStore.deleteItemAsync('jwt');
     }
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                {user ?
-                    <>
-                        <Stack.Screen 
-                            name="Home"
-                            options={{
-                                title: 'DDSS-DEMO • Home',
-                            }}
-                        >
-                            {props => <Home {...props} logout={() => logout()} />}
-                        </Stack.Screen>
-                    </>
-                    :
-                    <>
-                        <Stack.Screen
-                            name="SignIn"
-                            options={{
-                                title: 'DDSS-DEMO • Sign in',
-                            }}
-                        >
-                            {props => <SignIn {...props} login={login} />}
-                        </Stack.Screen>
-                        <Stack.Screen
-                            name="SignUp"
-                            options={{
-                                title: 'DDSS-DEMO • Sign up',
-                            }}
-                        >
-                            {props => <SignUp {...props} register={() => navigation.navigate("Home")} />}
-                        </Stack.Screen>
-                    </>
-                }
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+    setJwt('');
+  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {jwt ? (
+          <Stack.Screen
+            name="Home"
+            options={{
+              headerTitle: 'DDSS-DEMO • Home',
+              headerRight: () => (
+                <Button color="#2089DC" onPress={logout} title="Logout" />
+              ),
+            }}>
+            {(props) => <Home {...props} logout={() => logout()} jwt={jwt} />}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen
+              name="SignIn"
+              options={{
+                title: 'DDSS-DEMO • Sign in',
+              }}>
+              {(props) => <SignIn {...props} login={login} />}
+            </Stack.Screen>
+            <Stack.Screen
+              name="SignUp"
+              options={{
+                title: 'DDSS-DEMO • Sign up',
+              }}>
+              {(props) => (
+                <SignUp
+                  {...props}
+                  register={() => navigation.navigate('Home')}
+                />
+              )}
+            </Stack.Screen>
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;

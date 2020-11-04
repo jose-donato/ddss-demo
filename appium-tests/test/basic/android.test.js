41727059e8a7842931a2e61677c70c4b2a1ac633
jose-donato <43375532+jose-donato@users.noreply.github.com>
2020-11-04 22:40:44 +0000

    Update android.test.js

--- appium-tests/test/basic/android.test.js
+++ appium-tests/test/basic/android.test.js
@@ -30,29 +30,8 @@ describe('Testing ddss-demo', function () {
         await homepage_shodanQueryInput.waitForDisplayed(3000)
         const home_shodanQueryInputText = await homepage_shodanQueryInput.getText();
         assert.equal(home_shodanQueryInputText, "Shodan query...");
-    })
-
-    it('correct credentials redirect to homepage', async function () {
-        const signin_usernameInput = await client.$("~signin_usernameInput")
-        await signin_usernameInput.setValue("user1")
-        const signin_passwordInput = await client.$("~signin_passwordInput")
-        await signin_passwordInput.setValue("pass1")
-        const signin_button = await client.$("~signin_submitButton")
-        await signin_button.click()
-        const homepage_shodanQueryInput = await client.$("~home_shodanQueryInput")
-        await homepage_shodanQueryInput.waitForDisplayed(3000)
-        const home_shodanQueryInputText = await homepage_shodanQueryInput.getText();
-        assert.equal(home_shodanQueryInputText, "Shodan query...");
-    })
-    /*
-      it('press login button redirects to login form', async function() {
-        const button = await client.$("#loginButton")
-        await button.click()
-        const usernameInput = await client.$("#loginUsername")
-        const passwordInput = await client.$("#loginPassword")
-        assert.exists(usernameInput)
-        assert.exists(passwordInput)
-      })*/
+    });
+    
     /*it('should create and destroy a session', async function () {
         const res = await client.status();
         assert.isObject(res.build);
